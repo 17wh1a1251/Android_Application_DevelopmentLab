603a7b13d9a29e955c7771d34cc884afb14e6b90
Supraja P <52523375+17wh1a1251@users.noreply.github.com>
2021-02-10 03:38:47 +0530

    Add files via upload

---
+++ 5/NotificationView.java
@@ -0,0 +1,17 @@
+package com.example.aadlab
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+public class NotificationView extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_view);
+        textView = findViewById(R.id.textView);
+//getting the notification message
+        String message=getIntent().getStringExtra("message");
+        textView.setText(message);
+    }
+}
